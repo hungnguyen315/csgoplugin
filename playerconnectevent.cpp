22263c0d6c2c2a1481808c2c604b77e732146d8d
root <root@localhost.localdomain>
2016-08-08 16:40:23 +0700

    add playerconnectevent.cpp file

---
+++ playerconnectevent.cpp
@@ -0,0 +1,11 @@
+#include "playerconnectevent.h"
+
+void PlayerConnectEvent::FireGameEvent(IGameEvent *event)
+{
+
+}
+
+int PlayerConnectEvent::GetEventDebugID()
+{
+	return EVENT_DEBUG_ID_INIT;
+}
